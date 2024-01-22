764e439950d8a9bf5c128c9d0dfb266680c0f7e7
[Aquario Determinado] <aquario.determinado@gmail.com>
2024-01-22 17:19:32 +0000

    Nav Menu appering again;

--- src/views/components/PageTemplate/SideBar/NavMenu/NavMenu.js
+++ src/views/components/PageTemplate/SideBar/NavMenu/NavMenu.js
@@ -1,10 +1,14 @@
 import styles from './NavMenu.module.css';
-import React from "react";
-import {createElement} from "react";
-
-
+import React, {createElement} from "react";
 
 class NavMenu extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            navBar: "Loading."
+        }
+    }
 
     items = [ {name: "home", permission: "all"},
                                                     {name: "atribution", permission: "atributor"},
@@ -16,22 +20,23 @@ class NavMenu extends React.Component{
     checkAvailableNavButtons(permissions) {
         var buttons = [];
         for( let item in this.items){
-            if(permissions.includes(item.permission)){
-                buttons.add(createElement("li", {className: styles.navItem},
+            if(true){ //TODO: Verify permissions
+                var newButton = createElement("li", {className: styles.navItem},
                     createElement("button", {id: item.name + "-nav-button", className: styles.navButton},
-                    item.name)
-                ));
+                        item.name));
+                buttons.push(newButton);
             }
         }
         return buttons;
     }
 
     render() {
+
         const permissions = this.props.userPermissions;
-        const navItems = this.checkAvailableNavButtons(permissions);
+        this.state.navBar = this.checkAvailableNavButtons(permissions).map((item) => item);
         return (
             <ul className={styles.navbar}>
-                {navItems.map((item) => item)}
+                {this.state.navBar}
             </ul>
         );
     }
